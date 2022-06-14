d7546223c65ae1d8826fe171007de590d0bffe9f
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-14 20:56:09 +0900

    Add run.sh

---
+++ test/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+abspath="$(dirname $0)"
+binarypath="$abspath/../bin"
+a="$binarypath/a.out"
+
+$a
--- test/test_1.py
+++ test/test_1.py
@@ -26,14 +26,19 @@ import os
 
 def test_grep():
     absolute_filepath = os.path.dirname(os.path.abspath(__file__))
-    cp0 = subprocess.call(absolute_filepath + "/../bin/a.out", shell=True)
-    print(cp0)
-    out = subprocess.call(
+    # cp0 = subprocess.call(absolute_filepath + "/../bin/a.out", shell=True)
+    # print(cp0)
+    os.chdir(absolute_filepath)
+    subprocess.run("sh " + absolute_filepath + "/run.sh", shell=True)  # Run calculation
+    cp1 = subprocess.run(
         "cat test.out | awk '/ENERGY/{getline;print $2}$1 ~ /Total/{print $(NF - 1)}' | tr -s '\n'",
         shell=True,
     )
-    assert cp0 == out
-
+    out = subprocess.run(
+        "cat ref.out | awk '/ENERGY/{getline;print $2}$1 ~ /Total/{print $(NF - 1)}' | tr -s '\n'",
+        shell=True,
+    )
+    assert cp1.stdout == out.stdout
 
 if __name__ == "__main__":
     test_grep()
