e682e11f83e73be2cab6793e9518501fceeac4cc
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-16 00:31:52 +0900

    Change not to use run.sh

--- test/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-abspath="$(dirname $0)"
-binarypath="$abspath/../bin"
-a="$binarypath/a.out"
-
-$a
--- test/test_1.py
+++ test/test_1.py
@@ -1,35 +1,15 @@
 import subprocess
 import os
 
-# import pytest
-
-
-# def err() -> None:
-#     print("err")
-
-
-# @pytest.mark.default
-#def test2() -> None:
-#     if 1 == 1:
-#         err()
-#         exit(1)
-#     assert 0 == pytest.approx(10 ** -20)
-
-
-# @pytest.mark.performance
-# def test() -> None:
-#     absolute_filepath = os.path.dirname(os.path.abspath(__file__))
-#     cp = subprocess.run("ls " + absolute_filepath + "/test.sh", shell=True)
-#     assert cp.returncode == 0
-
-
 
 def test_grep():
     absolute_filepath = os.path.dirname(os.path.abspath(__file__))
     # cp0 = subprocess.call(absolute_filepath + "/../bin/a.out", shell=True)
     # print(cp0)
     os.chdir(absolute_filepath)
-    subprocess.run("sh " + absolute_filepath + "/run.sh", shell=True)  # Run calculation
+    # subprocess.run("sh " + absolute_filepath + "/run.sh", shell=True)  # Run calculation
+    binpath = os.path.normpath(os.path.join(absolute_filepath, "../bin/a.out"))
+    p = subprocess.run(binpath, shell=True)
     cp1 = subprocess.run(
         "cat test.out | awk '/ENERGY/{getline;print $2}$1 ~ /Total/{print $(NF - 1)}' | tr -s '\n'",
         shell=True,
@@ -39,6 +19,7 @@ def test_grep():
         shell=True,
     )
     assert cp1.stdout == out.stdout
+
 
 if __name__ == "__main__":
     test_grep()
